b5cbad38acf82111ae758fe72347203508f344b7
Miguel Alvarez <115565535+miguelgalvarez@users.noreply.github.com>
2024-06-11 09:19:48 -0400

    Update app.py
    Privacy changes

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.office365.com'  # Replace with your SMTP server
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'malvarez@esfestottawa.ca'
+app.config['MAIL_USERNAME'] = 'test@test.com'
 app.config['MAIL_PASSWORD'] = 'mail_password'
-app.config['MAIL_DEFAULT_SENDER'] = 'malvarez@esfestottawa.ca'
+app.config['MAIL_DEFAULT_SENDER'] = 'test@test.com'
 
 mail = Mail(app)
 
@@ -20,11 +20,11 @@ def index():
             selected_item = request.form.get('selected_option')
             selected_title = request.form.get('selected_title')  # Get the selected title
 
-            recipient_emails = 'malvarez@esfestottawa.ca'
+            recipient_emails = 'test@test.com'
 
             msg = Message('Demande de stockage', recipients=[recipient_emails])
             msg.body = f'Nouvelle commande pour {selected_item} dans la {selected_title}'
-            msg.add_recipient('private@esfestottawa.ca')
+            msg.add_recipient('private@email.com')
             mail.send(msg)
 
             # Redirect after processing the form
